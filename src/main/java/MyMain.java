317ca0cad85bae3c457a2f5e7e5b24dc5eba0b77
HEL3X <person_of_paradise@hotmail.com>
2021-09-29 20:05:12 -0700

    Math.random() practice.

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,8 +2,7 @@ public class MyMain {
     // Rolls a single die
     // Returns a random integer between 1 and 6
     public static int rollDie() {
-        // REPLACE YOUR CODE HERE
-        return -1;
+        return (int) ((Math.random() * 6.0) + 1);
     }
 
     // Calculate the probability of rolling at least one 6 when rolling 
@@ -15,28 +14,65 @@ public class MyMain {
 
         // Your code should roll 6 dice 10,000 times, so you should have a
         // for loop such as:
+        int count2 = 0;
         for (int i = 0; i < 10000; i++) {
-
+            int count1 = 0;
+            for (int j = 0; j < 6; j++) {
+                if (rollDie()==6){
+                    count1++;
+                }
+            }
+            if (count1 > 0){
+                count2++;
+            }
         }
 
-        return -1.0;
+        return (count2/100.0);
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int count2 = 0;
+        for (int i = 0; i < 10000; i++) {
+            int count1 = 0;
+            for (int j = 0; j < 12; j++) {
+                if (rollDie()==6){
+                    count1++;
+                }
+            }
+            if (count1 >= 2){
+                count2++;
+            }
+        }
+
+        return (count2/100.0);
     }
 
     // Calculate the probability of rolling at least three 6's when rolling
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int count2 = 0;
+        for (int i = 0; i < 10000; i++) {
+            int count1 = 0;
+            for (int j = 0; j < 18; j++) {
+                if (rollDie()==6){
+                    count1++;
+                }
+            }
+            if (count1 >= 3){
+                count2++;
+            }
+        }
+
+        return (count2/100.0);
     }
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        System.out.println("The probability of rolling at least one 6 when rolling six dice is: " + probabilityOneSix() + "%");
+        System.out.println("The probability of rolling at least two 6's when rolling twelve dice is: " + probabilityTwoSixes() + "%");
+        System.out.println("The probability of rolling at least three 6's when rolling eighteen dice is: " + probabilityThreeSixes() + "%");
+        System.out.println();
+        System.out.println("The probability of rolling at least one 6 when rolling six dice is the most probable!");
     }
 }
